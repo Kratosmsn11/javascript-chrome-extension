3d1ad8cef9f6e54f527188878e4daa7bf30529d5
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2022-09-27 20:57:06 -0700

    Chrome Extension

---
+++ index.js
@@ -0,0 +1,29 @@
+// chrome://extensions/
+let myLeads = []
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+console.log(ulEl)
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = ''
+    renderLeads()
+})
+
+
+function renderLeads(){
+    let listItems = ""
+    for(let i=0; i<myLeads.length;i++){
+        // listItems += "<li><a target='_blank' href='" + myLeads[i] +"'>" + myLeads[i] + "</a></li>"
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>`
+
+    }
+    ulEl.innerHTML = listItems
+}
